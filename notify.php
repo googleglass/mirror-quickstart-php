01470def4a0c86ed2b737cb1ae16572eed347619
mimming <jen@mimming.com>
2013-05-05 22:29:06 -0700

    Added timeline echo for photos and timeline notifications

--- notify.php
+++ notify.php
@@ -15,6 +15,34 @@
  * limitations under the License.
  */
 //  Author: Jenny Murphy - http://google.com/+JennyMurphy
+// Modified by: Winnie Tong
+
+
+
+// Always respond with a 200 right away and then terminate the connection to prevent notification
+// retries. How this is done depends on your HTTP server configs. I'll try a few common techniques
+// here, but if none of these work, start troubleshooting here.
+
+// First try: the content length header
+header("Content-length: 0");
+
+// Next, assuming it didn't work, attempt to close the output buffer by setting the time limit.
+ignore_user_abort(true);
+set_time_limit(0);
+
+// And one more thing to try: forking the heavy lifting into a new process. Yeah, crazy eh?
+if(function_exists('pcntl_fork')) {
+  $pid = pcntl_fork();
+  if ($pid == -1) {
+    error_log("could not fork!");
+    exit();
+  } else if ($pid) {
+    // fork worked! but I'm the parent. time to exit.
+    exit();
+  }
+}
+
+// In the child process (hopefully). Do the processing.
 require_once 'config.php';
 require_once 'mirror-client.php';
 require_once 'google-api-php-client/src/Google_Client.php';
@@ -22,16 +50,18 @@ require_once 'google-api-php-client/src/contrib/Google_MirrorService.php';
 require_once 'util.php';
 
 if($_SERVER['REQUEST_METHOD'] != "POST") {
-  http_send_status(400);
+  echo "method not supported";
   exit();
 }
 
 // Parse the request body
-$request = json_decode(http_get_request_body());
+$request_bytes = @file_get_contents('php://input');
+$request = json_decode($request_bytes, true);
 
 // A notification has come in. If there's an attached photo, bounce it back
 // to the user
 $user_id = $request['userToken'];
+
 $access_token = get_credentials($user_id);
 
 $client = get_google_api_client();
@@ -40,8 +70,34 @@ $client->setAccessToken($access_token);
 // A glass service for interacting with the Mirror API
 $mirror_service = new Google_MirrorService($client);
 
-$timeline_item = new Google_TimelineItem();
-$timeline_item->setText("Got a notification " . $request);
+switch($request['collection']) {
+  case "timeline":
+    $timeline_item_id = $request['itemId'];
 
-insertTimelineItem($mirror_service, $timeline_item, null, null);
-//TODO: if there's an attached photo, echo it
+    $timeline_item = new Google_TimelineItem($mirror_service->timeline->get($timeline_item_id));
+
+    $attachments = $timeline_item->getAttachments();
+    $attachment = $attachments[0];
+
+    $bytes = downloadAttachment($timeline_item_id, $attachment);
+
+    // Insert a new timeline card, with a copy of that photo attached
+    $echo_timeline_item = new Google_TimelineItem();
+    $echo_timeline_item->setText("Echoing your shared photo");
+    $echo_timeline_item->setNotification(
+      new google_NotificationConfig(array("level"=>"DEFAULT")));
+    insertTimelineItem($mirror_service, $echo_timeline_item, "image/jpeg", $bytes);
+    break;
+  case "locations":
+    $location = new Google_Location($mirror_service->locations->get("latest"));
+    // Insert a new timeline card, with a copy of that photo attached
+    $loc_timeline_item = new Google_TimelineItem();
+    $loc_timeline_item->setText("You are at " . $location->getLatitude() . " by " .
+        $location->getLongitude());
+
+    insertTimelineItem($mirror_service, $loc_timeline_item, null, null);
+    break;
+  default:
+    error_log("I don't know how to process this notification:" . $request);
+}
+
